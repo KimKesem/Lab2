f22a6efea978251249efb7f14430e72c05179f38
User <kristianshirman@gmail.com>
2024-01-02 22:39:16 +0600

    Initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Lab2.Model;
+using Lab2.View;
+using Lab2.Controller;
+
+namespace Lab2;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var tasks = new List<TaskItem>();
+        var view = new TaskView();
+        var controller = new TaskController(tasks, view);
+        controller.Run();
+    }
+}
